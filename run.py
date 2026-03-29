102400555ccc5608335f1bbfa53770b66ba0c15d
mascuud0715-arch <mascuud0715@gmail.com>
2026-03-29 18:36:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import threading
 import main
-import marketbot
+import market_bot   # ✅ sax
 
 def run_main():
     main.bot.infinity_polling()
 
 def run_market():
-    marketbot.bot.infinity_polling()
+    market_bot.bot.infinity_polling()
 
 t1 = threading.Thread(target=run_main)
 t2 = threading.Thread(target=run_market)
