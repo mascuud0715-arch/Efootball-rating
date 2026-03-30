54dbd3a18317c26841af94033d613d244d734b10
mascuud0715-arch <mascuud0715@gmail.com>
2026-03-30 13:17:05 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import threading
 import main
-import market_bot   # ✅ sax
+import market_bot
+import titan_bot
 
 def run_main():
     main.bot.infinity_polling()
@@ -8,8 +9,13 @@ def run_main():
 def run_market():
     market_bot.bot.infinity_polling()
 
+def run_titan():
+    titan_bot.bot.infinity_polling()
+
 t1 = threading.Thread(target=run_main)
 t2 = threading.Thread(target=run_market)
+t3 = threading.Thread(target=run_titan)
 
 t1.start()
 t2.start()
+t3.start()
